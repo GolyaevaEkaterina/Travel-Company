64c0a42c85f2e8966fafeedc25a3d1834e1bcce1
GolyaevaEkaterina <katrin.alien1@gmail.com>
2024-01-16 16:57:32 +0300

    дизайн

--- src/app.js
+++ src/app.js
@@ -15,11 +15,31 @@ async function renderTours() {
             new Date(tour.endTime),
             new Date(tour.startTime)
         )
-        container.innerHTML += `
-        <div class="bg-slate-200 shadow-2xl flex flex-col gap-y-3" id="container-basic">
-                        <div class="w-full h-full relative">
+
+        const city = tour.city
+
+        if (city == null){
+            container.innerHTML += `
+        <div class="tour" id="container-basic-${tour.id}">
+                        <div class="w-full h-72 relative">
                            <img class="tour-image" src="${tour.image}" />
-                           <div class="absolute inset-0 bg-black opacity-40" id="container-upper"></div>
+
+                           <div class="absolute inset-0 bg-black/50 z-10" id="container-upper-${tour.id}"></div>
+ 
+
+                           <div class="flex justify-between pt-1 absolute bg-teal-900/50 top-0 right-0 left-0 z-20 ">
+                             <p class="tour-hotel">${tour.hotelName}</p>
+                             <div class="flex gap-2 mr-5 text-xl items-center text-emerald-300">
+                               <i class="fa-solid fa-star"></i>
+                               <p class="tour-hotel-rating">${tour.rating}</p>
+                             </div>
+                           </div>
+
+                           <div class="country">
+                              <h2 class="tour-title">${tour.country}</h2>
+                              <h4 class="tour-city"></h2>
+                           </div>
+
                         </div>
                         <p class="tour-date"> ${format(
                             new Date(tour.startTime),
@@ -28,49 +48,92 @@ async function renderTours() {
                         )} - ${format(new Date(tour.endTime), "dd MMMM", {
                             locale: ru
                         })}  ${duration} дней</p>
-                        <h2 class="tour-title">${tour.country}</h2>
-                        <p class="tour-price">
-                        ${tour.price}
-                        </p>
-                        <p class="tour-hotel">${tour.hotelName}</p>
-                        <div class="flex justify-end mr-5">
-                            <button class="tour-button w-1/2">Подробнее</button>
-                            <button class="tour-button w-10">
-                                <i class="fa-regular fa-heart"></i>
-                            </button>
+                        
+                        <div class="flex justify-between ml-5 mr-5 items-center">
+                           <div class="flex items-center mb-5 text-slate-500">
+                             <i class="fa-solid fa-ruble-sign"></i>
+                             <p class="tour-price">${tour.price}</p>
+                           </div>
+                        
+                           <div class="flex justify-end">
+                               <button class="tour-button w-full">Подробнее</button>
+                               <button class="tour-button w-10">
+                                   <i class="fa-regular fa-heart"></i>
+                               </button>
+                           </div>
                         </div>
                         `
-    
-        // const basicContainer = document.getElementById('container-basic')
-        // basicContainer.addEventListener('mouseover', removeUpperContainer)
+        }else{
+        container.innerHTML += `
+        <div class="tour" id="container-basic-${tour.id}">
+                        <div class="w-full h-72 relative">
+                           <img class="tour-image" src="${tour.image}" />
 
-        // basicContainer.addEventListener('mouseout', removeReturnContainer)
-    
+                           <div class="absolute inset-0 bg-black/50 z-10" id="container-upper-${tour.id}"></div>
+ 
+
+                           <div class="flex justify-between pt-1 absolute bg-teal-900/50 top-0 right-0 left-0 z-20 ">
+                             <p class="tour-hotel">${tour.hotelName}</p>
+                             <div class="flex gap-2 mr-5 text-xl items-center text-emerald-300">
+                               <i class="fa-solid fa-star"></i>
+                               <p class="tour-hotel-rating">${tour.rating}</p>
+                             </div>
+                           </div>
+
+                           <div class="country">
+                               <h2 class="tour-title">${tour.country}.</h2>
+                               <h4 class="tour-city">${tour.city}</h2>
+                           </div>
+
+                        </div>
+                        <p class="tour-date"> ${format(
+                            new Date(tour.startTime),
+                            "dd MMMM",
+                            { locale: ru }
+                        )} - ${format(new Date(tour.endTime), "dd MMMM   ", {
+                            locale: ru
+                        })}  ${duration} дней</p>
+                        
+                        <div class="flex justify-between ml-5 mr-5 items-center">
+                           <div class="flex items-center mb-5 text-slate-500">
+                             <i class="fa-solid fa-ruble-sign"></i>
+                             <p class="tour-price">${tour.price}</p>
+                           </div>
+                        
+                           <div class="flex justify-end">
+                               <button class="tour-button w-full">Подробнее</button>
+                               <button class="tour-button w-10">
+                                   <i class="fa-regular fa-heart"></i>
+                               </button>
+                           </div>
+                        </div>
+                        
+                        
+                        `
+                    }            
     })
 
     tours.forEach((tour) => {
-        const basicContainer = document.getElementById(`container-basic-${book.id}`)
+        const basicContainer = document.getElementById(`container-basic-${tour.id}`)
         const makeRemoveUpperContainer = () => removeUpperContainer(tour.id)
         basicContainer.addEventListener('mouseover', makeRemoveUpperContainer)
   
-        
+        const upperContainer = document.getElementById(`container-upper-${tour.id}`)
+        const makeReturnUpperContainer = () => removeUpperContainer(tour.id)
+        basicContainer.addEventListener('mouseout', makeReturnUpperContainer)
   
-        // const buttonOpenFormEditBook = document.getElementById(`editBook__Open-form-${book.id}`)
-        // buttonOpenFormEditBook.addEventListener('click', () => {
-        //   currentEditBookId = book.id
-        //   openModalEditBook()
         })
 }
 
 
 
-function removeUpperContainer() {
-  const upperContainer = document.getElementById('container-upper')
+function removeUpperContainer(tour) {
+  const upperContainer = document.getElementById(`container-upper-${tour.id}`)
   upperContainer.style.display = "none"
 }
 
-function removeReturnContainer() {
-  const upperContainer = document.getElementById('container-upper')
+function returnUpperContainer(tour) {
+  const upperContainer = document.getElementById(`container-upper-${tour.id}`)
   upperContainer.style.display = "flex"
 }
 
